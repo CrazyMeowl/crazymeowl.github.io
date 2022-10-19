5bd0b56a939fc7d456173c78d9d51bc624e56603
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-10-19 12:56:26 +0700

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var key = "X9pkAse76Fgcvlf89qQhod0J5mkl16Fc";
 
 const getWeatherDaily = async (id) => {
 
-  var forecastUrl = `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${key}&details=false&language=vi-vn&metric=true`;
+  var forecastUrl = `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=${key}&details=false&language=vi-vn&metric=true`;
   var res = await fetch(forecastUrl);
   var data = await res.json();
   console.log("Daily Weather")
@@ -12,7 +12,7 @@ const getWeatherDaily = async (id) => {
 const getWeatherHourly = async (id) => {
 
   
-  var forecastUrl = `http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${id}?apikey=${key}&language=vi-vn&details=true&metric=true`;
+  var forecastUrl = `https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/${id}?apikey=${key}&language=vi-vn&details=true&metric=true`;
   var res = await fetch(forecastUrl);
   var data = await res.json();
   console.log("Hourly Weather")
