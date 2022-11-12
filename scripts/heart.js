72bce21caec6263f38e2203c3c1570137d771223
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2022-11-12 10:12:14 +0700

    a

--- scripts/heart.js
+++ scripts/heart.js
@@ -1,146 +1,190 @@
-window.requestAnimationFrame =
-    window.__requestAnimationFrame ||
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        (function () {
-            return function (callback, element) {
-                var lastTime = element.__lastTime;
-                if (lastTime === undefined) {
-                    lastTime = 0;
-                }
-                var currTime = Date.now();
-                var timeToCall = Math.max(1, 33 - (currTime - lastTime));
-                window.setTimeout(callback, timeToCall);
-                element.__lastTime = currTime + timeToCall;
-            };
-        })();
-window.isDevice = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(((navigator.userAgent || navigator.vendor || window.opera)).toLowerCase()));
-var loaded = false;
-var init = function () {
-    if (loaded) return;
-    loaded = true;
-    var mobile = window.isDevice;
-    var koef = mobile ? 0.5 : 1;
-    var canvas = document.getElementById('heart');
-    var ctx = canvas.getContext('2d');
-    var width = canvas.width = koef * innerWidth;
-    var height = canvas.height = koef * innerHeight;
-    var rand = Math.random;
-    ctx.fillStyle = "rgba(0,0,0,1)";
-    ctx.fillRect(0, 0, width, height);
+let particles = [];
+let microparticles = [];
 
-    var heartPosition = function (rad) {
-        //return [Math.sin(rad), Math.cos(rad)];
-        return [Math.pow(Math.sin(rad), 3), -(15 * Math.cos(rad) - 5 * Math.cos(2 * rad) - 2 * Math.cos(3 * rad) - Math.cos(4 * rad))];
-    };
-    var scaleAndTranslate = function (pos, sx, sy, dx, dy) {
-        return [dx + pos[0] * sx, dy + pos[1] * sy];
-    };
+const c1 = createCanvas({
+  width: $(window).width(),
+  height: $(window).height(),
+});
 
-    window.addEventListener('resize', function () {
-        width = canvas.width = koef * innerWidth;
-        height = canvas.height = koef * innerHeight;
-        ctx.fillStyle = "rgba(0,0,0,1)";
-        ctx.fillRect(0, 0, width, height);
-    });
+const tela = c1.canvas;
+const canvas = c1.context;
 
-    var traceCount = mobile ? 20 : 50;
-    var pointsOrigin = [];
-    var i;
-    var dr = mobile ? 0.3 : 0.1;
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 210, 13, 0, 0));
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 150, 9, 0, 0));
-    for (i = 0; i < Math.PI * 2; i += dr) pointsOrigin.push(scaleAndTranslate(heartPosition(i), 90, 5, 0, 0));
-    var heartPointsCount = pointsOrigin.length;
+// $("body").append(tela);
+$("body").append(c1.canvas);
 
-    var targetPoints = [];
-    var pulse = function (kx, ky) {
-        for (i = 0; i < pointsOrigin.length; i++) {
-            targetPoints[i] = [];
-            targetPoints[i][0] = kx * pointsOrigin[i][0] + width / 2;
-            targetPoints[i][1] = ky * pointsOrigin[i][1] + height / 2;
-        }
-    };
+class Particle1 {
+  constructor(canvas) {
+    this.random = Math.random();
+    this.random1 = Math.random();
+    this.random2 = Math.random();
+    this.progress = 0;
+    this.canvas = canvas;
+    this.life = 1000 + Math.random() * 3000;
 
-    var e = [];
-    for (i = 0; i < heartPointsCount; i++) {
-        var x = rand() * width;
-        var y = rand() * height;
-        e[i] = {
-            vx: 0,
-            vy: 0,
-            R: 2,
-            speed: rand() + 5,
-            q: ~~(rand() * heartPointsCount),
-            D: 2 * (i % 2) - 1,
-            force: 0.2 * rand() + 0.7,
-            f: "hsla(0," + ~~(40 * rand() + 60) + "%," + ~~(60 * rand() + 20) + "%,.3)",
-            trace: []
-        };
-        for (var k = 0; k < traceCount; k++) e[i].trace[k] = {x: x, y: y};
+    this.x =
+      $(window).width() / 2 + (Math.random() * 20 - Math.random() * 20);
+    this.y = $(window).height();
+    this.s = 2 + Math.random();
+    this.w = $(window).width();
+    this.h = $(window).height();
+    this.direction = this.random > 0.5 ? -1 : 1;
+    this.radius = 1 + 3 * this.random;
+    this.color = "#ff417d";
+
+    this.ID = setInterval(
+      function() {
+        microparticles.push(
+          new microParticle(c1.context, {
+            x: this.x,
+            y: this.y,
+          })
+        );
+      }.bind(this),
+      this.random * 20
+    );
+
+    setTimeout(
+      function() {
+        clearInterval(this.ID);
+      }.bind(this),
+      this.life
+    );
+  }
+
+  render() {
+    this.canvas.beginPath();
+    this.canvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    // this.canvas.lineWidth = 2;
+    this.canvas.shadowOffsetX = 0;
+    this.canvas.shadowOffsetY = 0;
+    // this.canvas.shadowBlur = 6;
+    this.canvas.shadowColor = "#000000";
+    this.canvas.fillStyle = this.color;
+    this.canvas.fill();
+    this.canvas.closePath();
+  }
+
+  move() {
+    this.x -=
+      this.direction *
+      Math.sin(this.progress / (this.random1 * 430)) *
+      this.s;
+    this.y -= Math.cos(this.progress / this.h) * this.s;
+
+    if (this.x < 0 || this.x > this.w - this.radius) {
+      clearInterval(this.ID);
+      return false;
     }
 
-    var config = {
-        traceK: 0.4,
-        timeDelta: 0.01
-    };
+    if (this.y < 0) {
+      clearInterval(this.ID);
+      return false;
+    }
+    this.render();
+    this.progress++;
+    return true;
+  }
+}
 
-    var time = 0;
-    var loop = function () {
-        var n = -Math.cos(time);
-        pulse((1 + n) * .5, (1 + n) * .5);
-        time += ((Math.sin(time)) < 0 ? 9 : (n > 0.8) ? .2 : 1) * config.timeDelta;
-        ctx.fillStyle = "rgba(0,0,0,.1)";
-        ctx.fillRect(0, 0, width, height);
-        for (i = e.length; i--;) {
-            var u = e[i];
-            var q = targetPoints[u.q];
-            var dx = u.trace[0].x - q[0];
-            var dy = u.trace[0].y - q[1];
-            var length = Math.sqrt(dx * dx + dy * dy);
-            if (10 > length) {
-                if (0.95 < rand()) {
-                    u.q = ~~(rand() * heartPointsCount);
-                }
-                else {
-                    if (0.99 < rand()) {
-                        u.D *= -1;
-                    }
-                    u.q += u.D;
-                    u.q %= heartPointsCount;
-                    if (0 > u.q) {
-                        u.q += heartPointsCount;
-                    }
-                }
-            }
-            u.vx += -dx / length * u.speed;
-            u.vy += -dy / length * u.speed;
-            u.trace[0].x += u.vx;
-            u.trace[0].y += u.vy;
-            u.vx *= u.force;
-            u.vy *= u.force;
-            for (k = 0; k < u.trace.length - 1;) {
-                var T = u.trace[k];
-                var N = u.trace[++k];
-                N.x -= config.traceK * (N.x - T.x);
-                N.y -= config.traceK * (N.y - T.y);
-            }
-            ctx.fillStyle = u.f;
-            for (k = 0; k < u.trace.length; k++) {
-                ctx.fillRect(u.trace[k].x, u.trace[k].y, 1, 1);
-            }
-        }
-        //ctx.fillStyle = "rgba(255,255,255,1)";
-        //for (i = u.trace.length; i--;) ctx.fillRect(targetPoints[i][0], targetPoints[i][1], 2, 2);
+class microParticle {
+  constructor(canvas, options) {
+    this.random = Math.random();
+    this.random1 = Math.random();
+    this.random2 = Math.random();
+    this.progress = 0;
+    this.canvas = canvas;
 
-        window.requestAnimationFrame(loop, canvas);
-    };
-    loop();
-};
+    this.x = options.x;
+    this.y = options.y;
+    this.s = 2 + Math.random() * 3;
+    this.w = $(window).width();
+    this.h = $(window).height();
+    this.radius = 1 + this.random * 0.5;
+    this.color = "#4EFCFE"; //this.random > .5 ? "#a9722c" : "#FFFED7"
+  }
 
-var s = document.readyState;
-if (s === 'complete' || s === 'loaded' || s === 'interactive') init();
-else document.addEventListener('DOMContentLoaded', init, false);
+  render() {
+    this.canvas.beginPath();
+    this.canvas.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    this.canvas.lineWidth = 2;
+    this.canvas.fillStyle = this.color;
+    this.canvas.fill();
+    this.canvas.closePath();
+  }
+
+  move() {
+    this.x -=
+      Math.sin(this.progress / (100 / (this.random1 - this.random2 * 10))) *
+      this.s;
+    this.y += Math.cos(this.progress / this.h) * this.s;
+
+    if (this.x < 0 || this.x > this.w - this.radius) {
+      return false;
+    }
+
+    if (this.y > this.h) {
+      return false;
+    }
+    this.render();
+    this.progress++;
+    return true;
+  }
+}
+
+var random_life = 1000;
+
+setInterval(
+  function() {
+    particles.push(new Particle1(canvas));
+    random_life = 2000 * Math.random();
+  }.bind(this),
+  random_life
+);
+
+function clear() {
+  let grd = canvas.createRadialGradient(
+    tela.width / 2,
+    tela.height / 2,
+    0,
+    tela.width / 2,
+    tela.height / 2,
+    tela.width
+  );
+  grd.addColorStop(0, "rgba(20,20,20,1)");
+  grd.addColorStop(1, "rgba(0,0,0,0)");
+  // Fill with gradient
+  canvas.globalAlpha = 0.16;
+  canvas.fillStyle = grd;
+  canvas.fillRect(0, 0, tela.width, tela.height);
+}
+
+function blur(ctx, canvas, amt) {
+  // ctx.filter = `blur(${amt}px)`
+  // ctx.drawImage(canvas, 0, 0)
+  // ctx.filter = 'none'
+}
+
+function update() {
+  clear();
+  particles = particles.filter(function(p) {
+    return p.move();
+  });
+  microparticles = microparticles.filter(function(mp) {
+    return mp.move();
+  });
+  requestAnimationFrame(update.bind(this));
+}
+
+function createCanvas(properties) {
+  let canvas = document.createElement("canvas");
+  canvas.width = properties.width;
+  //   canvas.style.zIndex = 999;
+  canvas.height = properties.height;
+  let context = canvas.getContext("2d");
+  return {
+    canvas: canvas,
+    context: context,
+  };
+}
+
+update();
